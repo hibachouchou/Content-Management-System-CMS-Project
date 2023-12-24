ec3f56ebdf948aeebe15b5e2d24bb87e2af3fbe7
Hiba <91759254+hibachouchou@users.noreply.github.com>
2023-12-24 23:58:18 +0100

    Commit4

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -2,7 +2,9 @@
 @section('main')
 <div>
     <h2 class="text-5xl font-extrabold dark:text-white mb-8">Categories</h2>
-    <button type="button" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Add Category</button>
+    <button type="button" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+    Add Category
+   </button>
 
 
 <!-- Main modal -->
@@ -34,8 +36,8 @@
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Author</label>
                         <input type="text"  id="author" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Autor" required="" name="author">
                     </div>
+                </div>
                 <button type="submit" class="text-white inline-flex items-center bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                     Add New Category
                 </button>
             </form>
@@ -77,14 +79,8 @@
     </button>
                 </td>
             </tr>
-            @endforeach
-     
-        </tbody>
-    </table>
-</div>
 
-@foreach ($categories as $category)
-<!-- Main modal -->
+            <!-- Main modal -->
 <div id="crud-modal2-{{ $category->id }}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <!-- Modal content -->
@@ -102,7 +98,8 @@
                 </button>
             </div>
             <!-- Modal body -->
-                <form action="/update_category/{{ $category->id }}" class="p-4 md:p-5" method="POST">
+            <div class="p-4 md:p-5">
+                <form action="/update_category/{{ $category->id }}"  method="POST">
                     @csrf
                     @method('PUT')
                     <div class="grid gap-4 mb-4 grid-cols-2">
@@ -118,6 +115,7 @@
                         Update Category
                     </button>
                 </form>
+            </div>
         </div>
     </div>
 </div> 
@@ -150,7 +148,15 @@
         </div>
     </div>
 </div>
-@endforeach
+            @endforeach
+     
+        </tbody>
+    </table>
+</div>
+
+
+
+
 @if(session('msg'))
     <script>
         var alertType = "{{ session('alert', 'info') }}"; // Default to 'info' if not set
